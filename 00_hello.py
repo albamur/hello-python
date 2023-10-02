2df1da7df6133848016964b165fcff8041a302c9
albamunnoz <albamrpz@gmail.com>
2023-10-02 21:33:15 +0200

    Primeras clases del curso

---
+++ 00_hello.py
@@ -0,0 +1,26 @@
+# Python es interpretado como JS
+# Esto es un comentario
+
+"""
+Esto es un comentario 
+en varias
+lineas
+"""
+
+'''
+Esto también es un comentario 
+en varias
+lineas
+'''
+
+print("Hola Python")
+
+# Las cadenas de texto también pueden ir entre comillas simples ''
+print('Hola Python')
+
+# Consultar el tipo de dato
+print(type("Soy un dato str")) # Tipo 'str'
+print(type(5)) # Tipo 'int'
+print(type(1.5)) # Tipo 'float'
+print(type(3 + 2j)) # Tipo 'complex'
+print(type(True)) # Tipo 'boolean'
