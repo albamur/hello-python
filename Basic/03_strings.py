c450ac8368ac2b941fb834c54e832a1f707b1129
albamunnoz <albamrpz@gmail.com>
2023-10-23 00:50:53 +0200

    titulos strings

--- Basic/03_strings.py
+++ Basic/03_strings.py
@@ -1,39 +1,39 @@
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> STRINGS <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< 
 
+# ---------------------------------- ASIGNACIÓN ---------------------------------- 
 my_string = "Mi String"
 my_other_string = 'Mi otro String'
+
+# ---------------------------------- POSICIONES ---------------------------------- 
+# En Python, las cadenas son tratadas como matrices de caracteres. 
+# Esto significa que cada carácter en la cadena tiene un índice numérico que indica su posición
+print(my_string[0])  # Salida: 'M'
+print(my_string[3])  # Salida: 'S'
+
+# ---------------------------------- FUNCIÓN LEN() ---------------------------------- 
+# Se utiliza para obtener la longitud de una cadena, es decir, la cantidad de caracteres que contiene.
 
 print(len(my_string))
 print(len(my_other_string))
 print(my_string + " " + my_other_string)
 
-my_new_line_string = "Este es un String\ncon salto de línea"
-print(my_new_line_string)
 
-my_tab_string = "\tEste es un String con tabulación"
-print(my_tab_string)
+# ---------------------------------- DIVISIÓN O SLICING DE CADENAS ---------------------------------- 
+# El slicing es una técnica que nos permite extraer subcadenas de una cadena más grande. 
+# Esto se logra especificando un rango de índices que representan los límites de la subcadena que deseamos extraer.
 
-my_scape_string = "\\tEste es un String \\n escapado"
-print(my_scape_string)
+language = "Python"
 
+# Desde el principio
+principio = language[0:3] # Salida : Pyth
+print(principio)
 
-# Desempaquetado de caracteres
+# Hasta el final
+final = language[1:]  # Salida : ython
+print(final)
 
-language = "python"
-a, b, c, d, e, f = language
-print(a)
-print(e)
-
-# División
-
-language_slice = language[1:3]
-print(language_slice)
-
-language_slice = language[1:]
-print(language_slice)
-
-language_slice = language[-2]
-print(language_slice)
+ultimo_char = language[-1]
+print(ultimo_char)
 
 language_slice = language[0:6:2]
 print(language_slice)
@@ -43,6 +43,25 @@ print(language_slice)
 
 reversed_language = language[::-1]
 print(reversed_language)
+
+
+# Desempaquetado de caracteres
+a, b, c, d, e, f = language
+print(a)
+print(e)
+
+
+
+#FORMATEO
+my_new_line_string = "Este es un String\ncon salto de línea"
+print(my_new_line_string)
+
+my_tab_string = "\tEste es un String con tabulación"
+print(my_tab_string)
+
+my_scape_string = "\\tEste es un String \\n escapado"
+print(my_scape_string)
+
 
 
 # Funciones del sistema
